a5c7ea50233e33a887efa1c3c0f0b7bf5fd01e2e
aleatorik <vates0328@gmail.com>
2020-04-18 00:09:38 +0900

    feat: Create brush size and filling Mode

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d"); //context is how we manipulate pixels in canvas
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
-canvas.width = 600;
-canvas.height = 600;
+const INITIAL_COLOR = "2c2c2c";
+const CANVAS_SIZE = 600;
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = "INITIAL_COLOR";
+ctx.fillStyle = "INITIAL_COLOR";
 ctx.lineWidth = 2.5; // controls__range in index.html
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
   painting = false;
@@ -31,14 +39,15 @@ function onMouseMove(event) {
   }
 }
 
-function onMouseDown(event) {
-  //--when clicking
-  painting = true;
-}
-
 function onMouseUp(event) {
   //--when stopping keep clicking
   stopPainting();
+}
+
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
 }
 
 if (canvas) {
@@ -46,4 +55,39 @@ if (canvas) {
   canvas.addEventListener("mousedown", startPainting); //mousedown --when clicking
   canvas.addEventListener("mouseup", stopPainting); //mouseup --when stopping keep clicking
   canvas.addEventListener("mouseleave", stopPainting); //mouseleave --when mouse off to the canvas
+  canvas.addEventListener("click", handleCanvasClick);
+}
+
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
+
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handleColorClick)
+);
+
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+if (range) {
+  //if문 안에 이벤트를 일으키는 이유는 range라는 element가 존재하는지 확인할 수 있는 안전성 측면 때문
+  range.addEventListener("input", handleRangeChange);
+}
+
+function handleModeClick() {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
 }
