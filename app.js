4c2cfe7eae7e860336e92347bab04f308f1e7f3e
aleatorik <vates0328@gmail.com>
2020-04-21 17:03:26 +0900

    feat: Add function of the clear button

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 const saveBtn = document.getElementById("jsSave");
+const clearBtn = document.getElementById("jsClear");
 
 const INITIAL_COLOR = "2c2c2c";
 const CANVAS_SIZE = 600;
@@ -110,4 +111,12 @@ function handleSaveClick() {
 
 if (saveBtn) {
   saveBtn.addEventListener("click", handleSaveClick);
+}
+
+function handleClearClick() {
+  ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+}
+
+if (clearBtn) {
+  clearBtn.addEventListener("click", handleClearClick);
 }
